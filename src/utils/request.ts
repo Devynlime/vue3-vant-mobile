26298bf1381b26d7e948e8342500cbd29605edec
devynlime <1027880379@qq.com>
2024-12-17 16:21:49 +0800

    feat: 更新版本号至1.0.20，调整环境配置和请求头
    - 将 VITE_APP_PUBLIC_PATH 修改为根路径，确保资源加载路径正确
    - 更新版本号至 1.0.20，反映最新的功能和修复
    - 修改请求头常量名称为 'CableToken'，以增强代码可读性

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -7,7 +7,7 @@ import { IGW_USER_KEY, STORAGE_TOKEN_KEY } from '@/stores/mutation-type'
 // 可以根据自己的需要修改，常见的如 Access-Token，Authorization
 // 需要注意的是，请尽量保证使用中横线`-` 来作为分隔符，
 // 避免被 nginx 等负载均衡器丢弃了自定义的请求头
-export const REQUEST_TOKEN_KEY = 'Authorization'
+export const REQUEST_TOKEN_KEY = 'CableToken'
 export const IGW_TOKEN_KEY = 'AuthToken'
 
 // 创建 axios 实例
